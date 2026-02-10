5bf6978c7a9c4b4475608e3c4a570491c520f6e5
Matthew Clarke <m.d.clarke.sa@gmail.com>
2026-02-10 15:03:06 +0200

    add: load Scheduler Pro data

--- src/main.js
+++ src/main.js
@@ -1,35 +1,79 @@
-import { Client, Users } from 'node-appwrite';
+import { Client, TablesDB, Query, ID } from 'node-appwrite';
 
-// This Appwrite function will be executed every time your function is triggered
-export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+const PROJECT_ID = process.env.PROJECT_ID;
+const DATABASE_ID = process.env.DATABASE_ID;
+const RESOURCES_TABLE_ID = process.env.RESOURCES_TABLE_ID;
+const EVENTS_TABLE_ID = process.env.EVENTS_TABLE_ID;
+const ASSIGNMENTS_TABLE_ID = process.env.ASSIGNMENTS_TABLE_ID;
+const DEPENDENCIES_TABLE_ID = process.env.DEPENDENCIES_TABLE_ID;
+const CALENDARS_TABLE_ID = process.env.CALENDARS_TABLE_ID;
 
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
+export default async ({ req, res }) => {
+    const client = new Client()
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject(PROJECT_ID)
+        .setJWT(req.headers['authorization']);
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
+    const tablesDB = new TablesDB(client);
 
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+    if (req.method === 'OPTIONS') {
+          return res.send('', 200, {
+              'Access-Control-Allow-Origin'  : 'http://localhost:3000',
+              'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS',
+              'Access-Control-Allow-Headers' : 'Content-Type, Authorization',
+          })
+    }
+    if (req.method === 'GET') {
+        try {
+            const [
+                resourcesRes,
+                eventsRes,
+                assignmentsRes,
+                dependenciesRes,
+                calendarsRes
+            ] = await Promise.all([
+                tablesDB.listRows(DATABASE_ID, RESOURCES_TABLE_ID),
+                tablesDB.listRows(DATABASE_ID, EVENTS_TABLE_ID),
+                tablesDB.listRows(DATABASE_ID, ASSIGNMENTS_TABLE_ID),
+                tablesDB.listRows(DATABASE_ID, DEPENDENCIES_TABLE_ID),
+                tablesDB.listRows(DATABASE_ID, CALENDARS_TABLE_ID)
+            ]);
+
+            function cleanRow(row) {
+                row.id = row.$id;
+                const obj = Object.fromEntries(
+                    Object.entries(row)
+                        .filter(([_, v]) => v != null)
+                        .filter(([k]) => k[0] !== '$')
+                );
+                // Parse JSON string fields back to objects
+                ['intervals', 'exceptionDates', 'segments'].forEach((field) => {
+                    if (typeof obj[field] === 'string') {
+                        try { obj[field] = JSON.parse(obj[field]); } catch (e) { /* keep as string */ }
+                    }
+                });
+                return obj;
+            }
+
+            return res.json({
+                success      : true,
+                resources    : { rows : resourcesRes.documents.map(cleanRow) },
+                events       : { rows : eventsRes.documents.map(cleanRow) },
+                assignments  : { rows : assignmentsRes.documents.map(cleanRow) },
+                dependencies : { rows : dependenciesRes.documents.map(cleanRow) },
+                calendars    : { rows : calendarsRes.documents.map(cleanRow) }
+            }, 200, {
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+            });
+        }
+        catch(err) {
+            return res.json({
+                success : false,
+                message : 'Scheduler Pro data could not be loaded'
+            }, 500, {
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+            });
+        }
+    }
+
 };
